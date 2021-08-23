9b5191172bbd2875a9d4b013325d7935689a09d5
Kenneth Ashley <ken.u.diggitt@gmail.com>
2021-08-22 22:19:26 -0400

    current work

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="/image.png" className="App-logo" alt="logo" />
+        <div className="App-body">
+          <p>
+            Has Vex launched yet?
+          </p>
+          <h3 className="No">No!</h3>
+        </div>
       </header>
     </div>
   );
