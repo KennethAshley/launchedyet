e81807a7dedffbe9bc6fdc4edecf27f90f657165
Kenneth Ashley <ken.u.diggitt@gmail.com>
2021-08-22 22:45:07 -0400

    rick it up

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="App-body">
+          <iframe
+            width="854"
+            height="480"
+            src="https://www.youtube.com/embed/dQw4w9WgXcQ?&autoplay=1"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen></iframe>
           <h3 className="No">No!</h3>
         </div>
       </header>
