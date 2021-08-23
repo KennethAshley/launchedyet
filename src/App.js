4435bbabf3d21e44aeacab86684c14231d2f1fdc
Kenneth Ashley <ken.u.diggitt@gmail.com>
2021-08-22 22:28:20 -0400

    remove

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/image.png" className="App-logo" alt="logo" />
         <div className="App-body">
-          <p>
-            Has Vex launched yet?
-          </p>
           <h3 className="No">No!</h3>
         </div>
       </header>
